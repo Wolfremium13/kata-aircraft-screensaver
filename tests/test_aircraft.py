7a531e8501fb8936907bed82b6bafd63137a3cd2
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-04-03 21:47:28 +0100

    ✅ test_aircraft_bounces_at_the_territory_west_border

--- tests/test_aircraft.py
+++ tests/test_aircraft.py
@@ -73,6 +73,16 @@ class TestAircraft(unittest.TestCase):
         assert_that(an_aircraft.current_position()).is_equal_to(
             Position(longitude=1, latitude=0)
         )
+    
+    def test_aircraft_bounces_at_the_territory_west_border(self):
+        territory = Territory(max_longitude=1, max_latitude=0)
+        an_aircraft = Aircraft.create(Position(longitude=1, latitude=0), territory)
+
+        an_aircraft.move(Direction.West)
+
+        assert_that(an_aircraft.current_position()).is_equal_to(
+            Position(longitude=0, latitude=0)
+        )
 
     def test_aircraft_bounces_at_the_territory_northern_border(self):
         territory = Territory(max_longitude=6, max_latitude=6)
