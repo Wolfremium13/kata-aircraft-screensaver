50553d5cc6ef309436503236b1dc2764caa5e97f
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-04-04 22:10:37 +0100

    ♻️ parametrize movement tests

--- tests/test_aircraft_movement.py
+++ tests/test_aircraft_movement.py
@@ -1,86 +1,36 @@
-import unittest
+import pytest
 
 from assertpy import assert_that
 
 from src.screensaver.aircraft import Aircraft
 from src.screensaver.direction import Direction
+from src.screensaver.movement import Movement
 from src.screensaver.position import Position
 from src.screensaver.territory import Territory
 
 
-class AircraftMovementWithDirectionShould(unittest.TestCase):
-    def setUp(self) -> None:
-        self.territory = Territory()
-        return super().setUp()
+TERRITORY = Territory(max_longitude=10, max_latitude=10)
+AIRCRAFT_POSITION = Position(longitude=5, latitude=5)
 
-    def test_aircraft_moves_north(self):
-        an_aircraft = Aircraft.create(Position(longitude=0, latitude=1), self.territory)
 
-        an_aircraft.move(Direction.North)
+@pytest.mark.parametrize(
+    "direction,expected_position",
+    [
+        (Direction.North, Movement(AIRCRAFT_POSITION).go_up),
+        (Direction.South, Movement(AIRCRAFT_POSITION).go_down),
+        (Direction.East, Movement(AIRCRAFT_POSITION).go_right),
+        (Direction.West, Movement(AIRCRAFT_POSITION).go_left),
+        (Direction.NorthEast, Movement(AIRCRAFT_POSITION).go_up_right),
+        (Direction.NorthWest, Movement(AIRCRAFT_POSITION).go_up_left),
+        (Direction.SouthEast, Movement(AIRCRAFT_POSITION).go_down_right),
+        (Direction.SouthWest, Movement(AIRCRAFT_POSITION).go_down_left),
+    ],
+)
+def test_aircraft_moves_based_on_a_certain_direction_should(
+    direction: Direction, expected_position: Position
+):
+    an_aircraft = Aircraft.create(AIRCRAFT_POSITION, TERRITORY)
 
-        assert_that(an_aircraft.current_position()).is_equal_to(
-            Position(longitude=0, latitude=2)
-        )
+    an_aircraft.move(direction)
 
-    def test_aircraft_moves_south(self):
-        an_aircraft = Aircraft.create(Position(longitude=0, latitude=1), self.territory)
-
-        an_aircraft.move(Direction.South)
-
-        assert_that(an_aircraft.current_position()).is_equal_to(
-            Position(longitude=0, latitude=0)
-        )
-
-    def test_aircraft_moves_east(self):
-        an_aircraft = Aircraft.create(Position(longitude=1, latitude=0), self.territory)
-
-        an_aircraft.move(Direction.East)
-
-        assert_that(an_aircraft.current_position()).is_equal_to(
-            Position(longitude=2, latitude=0)
-        )
-
-    def test_aircraft_moves_west(self):
-        an_aircraft = Aircraft.create(Position(longitude=1, latitude=0), self.territory)
-
-        an_aircraft.move(Direction.West)
-
-        assert_that(an_aircraft.current_position()).is_equal_to(
-            Position(longitude=0, latitude=0)
-        )
-
-    def test_aircraft_moves_north_west(self):
-        an_aircraft = Aircraft.create(Position(longitude=1, latitude=1), self.territory)
-
-        an_aircraft.move(Direction.NorthWest)
-
-        assert_that(an_aircraft.current_position()).is_equal_to(
-            Position(longitude=0, latitude=2)
-        )
-
-    def test_aircraft_moves_north_east(self):
-        an_aircraft = Aircraft.create(Position(longitude=0, latitude=0), self.territory)
-
-        an_aircraft.move(Direction.NorthEast)
-
-        assert_that(an_aircraft.current_position()).is_equal_to(
-            Position(longitude=1, latitude=1)
-        )
-
-    def test_aircraft_moves_south_east(self):
-        an_aircraft = Aircraft.create(Position(longitude=1, latitude=1), self.territory)
-
-        an_aircraft.move(Direction.SouthEast)
-
-        assert_that(an_aircraft.current_position()).is_equal_to(
-            Position(longitude=2, latitude=0)
-        )
-
-    def test_aircraft_moves_south_west(self):
-        an_aircraft = Aircraft.create(Position(longitude=1, latitude=1), self.territory)
-
-        an_aircraft.move(Direction.SouthWest)
-
-        assert_that(an_aircraft.current_position()).is_equal_to(
-            Position(longitude=0, latitude=0)
-        )
+    assert_that(an_aircraft.current_position()).is_equal_to(expected_position)
