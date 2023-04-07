d26ac486424c46f19ee87e9f9acb6f901a114782
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-04-07 14:18:18 +0100

    ❌ test_bounce_diagonally_at_the_cardinal_territory_borders

--- tests/test_aircraft_bounce.py
+++ tests/test_aircraft_bounce.py
@@ -47,3 +47,62 @@ def test_bounce_back_at_the_ordinal_territory_borders(
     an_aircraft.move(direction)
 
     assert_that(an_aircraft.current_position()).is_equal_to(expected_position)
+
+
+TERRITORY_WITH_LONGITUDE = Territory(max_longitude=10, max_latitude=0)
+TERRITORY_WITH_LATITUDE = Territory(max_longitude=0, max_latitude=10)
+
+
+@pytest.mark.parametrize(
+    "direction,expected_position,territory",
+    [
+        (
+            Direction.NorthWest,
+            Movement(AIRCRAFT_POSITION).go_up(),
+            TERRITORY_WITH_LATITUDE,
+        ),
+        (
+            Direction.NorthWest,
+            Movement(AIRCRAFT_POSITION).go_left(),
+            TERRITORY_WITH_LONGITUDE,
+        ),
+        (
+            Direction.SouthWest,
+            Movement(AIRCRAFT_POSITION).go_down(),
+            TERRITORY_WITH_LATITUDE,
+        ),
+        (
+            Direction.SouthWest,
+            Movement(AIRCRAFT_POSITION).go_left(),
+            TERRITORY_WITH_LONGITUDE,
+        ),
+        (
+            Direction.SouthEast,
+            Movement(AIRCRAFT_POSITION).go_down(),
+            TERRITORY_WITH_LATITUDE,
+        ),
+        (
+            Direction.SouthEast,
+            Movement(AIRCRAFT_POSITION).go_right(),
+            TERRITORY_WITH_LONGITUDE,
+        ),
+        (
+            Direction.NorthEast,
+            Movement(AIRCRAFT_POSITION).go_up(),
+            TERRITORY_WITH_LATITUDE,
+        ),
+        (
+            Direction.NorthEast,
+            Movement(AIRCRAFT_POSITION).go_right(),
+            TERRITORY_WITH_LONGITUDE,
+        ),
+    ],
+)
+def test_bounce_diagonally_at_the_cardinal_territory_borders(
+    direction: Direction, expected_position: Position, territory: Territory
+):
+    an_aircraft = Aircraft.create(AIRCRAFT_POSITION, territory)
+
+    an_aircraft.move(direction)
+
+    assert_that(an_aircraft.current_position()).is_equal_to(expected_position)
